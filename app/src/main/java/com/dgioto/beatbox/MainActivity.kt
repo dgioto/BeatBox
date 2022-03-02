e0ac2f3a563ca3d4979e487931466dd21fc9030b
Alexander Tih <dgioto@gmail.com>
2022-03-03 00:04:48 +0200

    Simple data binding

--- app/src/main/java/com/dgioto/beatbox/MainActivity.kt
+++ app/src/main/java/com/dgioto/beatbox/MainActivity.kt
@@ -2,10 +2,48 @@ package com.dgioto.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dgioto.beatbox.databinding.ActivityMainBinding
+import com.dgioto.beatbox.databinding.ListItemSoundBinding
+
+//page 417
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    val binding: ActivityMainBinding =
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter()
+        }
     }
+
+    private inner class SoundHolder(binding: ListItemSoundBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private inner class SoundAdapter : RecyclerView.Adapter<SoundHolder>() {
+
+
+        override fun getItemCount() = 0
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+            return  SoundHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) { }
+    }
+
+
 }
