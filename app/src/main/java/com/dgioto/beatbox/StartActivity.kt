cd7f2cfab88b1ae2041b15f73336111e33f587cd
dgioto <dgioto@gmail.com>
2023-06-28 21:44:33 +0300

    added StartActivity with name and load

---
+++ app/src/main/java/com/dgioto/beatbox/StartActivity.kt
@@ -0,0 +1,32 @@
+package com.dgioto.beatbox
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+
+    private val SPLASH_DELAY: Long = 5000 // Задержка в N секунд
+
+    private val splashHandler = Handler()
+    private val splashRunnable = Runnable {
+        val intent = Intent(this@StartActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        // Запускаем MainActivity после паузы
+        splashHandler.postDelayed(splashRunnable, SPLASH_DELAY)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Очищаем задачи из Handler для предотвращения утечки памяти
+        splashHandler.removeCallbacks(splashRunnable)
+    }
+}
